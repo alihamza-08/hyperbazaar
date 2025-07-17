e14cd2b0c6b6a47112df8959dbf74ad2f914ad99
alihamza-08 <alihamza760760@gmail.com>
2025-07-17 22:55:18 +0500

    final

--- hyperbazaar/frontendweb/grocerry/vite.config.js
+++ hyperbazaar/frontendweb/grocerry/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // <- This is required for Docker/EC2
-    port: 5173       // Optional: explicitly set the port
+    host: true,       // <- This makes the dev server accessible externally
+    port: 5173        // <- This should match the internal port Docker uses
   }
 })
